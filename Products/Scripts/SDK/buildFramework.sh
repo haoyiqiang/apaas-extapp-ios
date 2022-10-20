06635748dd63b5714492dc80f9e72682b5456965
Lyy <liuyuanyuan02@agora.io>
2022-10-20 17:47:37 +0800

    [ENH]: update ci with absolute path

--- Products/Scripts/SDK/buildFramework.sh
+++ Products/Scripts/SDK/buildFramework.sh
@@ -4,9 +4,9 @@ Res='\033[0m'
 
 SDK_Name=$1
 
-Current_Path=`pwd`
-SDKs_Path="../../../SDKs"
-Products_Root_Path="../../Libs"
+Current_Path=$(cd "$(dirname "$0")";pwd)
+SDKs_Path="$Current_Path/../../../SDKs"
+Products_Root_Path="$Current_Path/../../Libs"
 Products_Path="$Products_Root_Path/$SDK_Name"
 Builder_Path="${SDKs_Path}/AgoraBuilder"
 
@@ -18,15 +18,14 @@ rm -rf ${Products_Path}
 mkdir ${Products_Path}
 
 errorExit() {
-    SDK_Name=$1
-    Build_Result=$2
+    Build_Result=$1
 
     if [ $Build_Result != 0 ]; then
-        echo "SDK_Name: ${SDK_Name}"
-        exit 1
+        echo "${Color} ======${SDK_Name} Fails======== ${Res}"
+        exit -1
     fi
-    echo "build result: $Build_Result"
-    echo "${SDK_Name} build success"
+
+    echo "${Color} ======${SDK_Name} Succeeds======== ${Res}"
 }
 
 dependencyCheck() {
@@ -43,17 +42,35 @@ dependencyCheck() {
         
         libName=`echo $line | sed "s:pod\'\(.*\)\/Binary.*:\1:g"`
         repoPath=`echo $line | sed "s:.*\:path=>\'\(.*\)\/$libName.*\':\1:g"`
+
+        repoAbsolutePath=$Builder_Path/$repoPath
+
+        echo "$SDK_Name dependency repo:$repoAbsolutePath"
+        dependencyPath="$repoAbsolutePath/Products/Libs/$libName/$libName.framework"
         
-        dependencyPath="$repoPath/Products/Libs/$libName/$libName.framework"
-        
-        # call buildframework of dependency
-        if [ ! -f $dependencyPath ]; then
-            echo $dependencyPath
-            exit 1
-            cd $repoPath/Products/Scripts/SDK
-            sh buildframework.sh $libName
-            cd $Current_Path
+         # dependency check
+        if [ -d $dependencyPath ]; then
+            continue
         fi
+        
+        # call buildframework
+        echo "dependencyPath: $dependencyPath not found"
+        echo "$SDK_Name call build: $libName"
+
+        dependencySDKPath="$repoAbsolutePath/Products/Scripts/SDK"
+        if [ ! -d $dependencySDKPath ]; then
+            pwd
+            echo "dependencySDKPath not found: $dependencySDKPath"
+            exit
+        fi
+
+       sh $dependencySDKPath/buildframework.sh $libName
+
+        if [ $? -ne 0 ];then
+            exit
+        fi
+
+        cd $Current_Path
     done
     
     cd $Current_Path
@@ -66,4 +83,4 @@ dependencyCheck
 # current path is under Products/Scripts/SDK
 ./buildExecution.sh $Builder_Path ${SDK_Name} Release
 
-errorExit ${SDK_Name} $?
+errorExit $?
