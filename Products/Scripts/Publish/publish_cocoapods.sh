259a826b39bf08079dfd88cce64422d6659c2362
cavansu <suruoxi@agora.io>
2022-11-17 15:43:09 +0800

    [ENH]:updated script

--- Products/Scripts/Publish/publish_cocoapods.sh
+++ Products/Scripts/Publish/publish_cocoapods.sh
@@ -27,6 +27,7 @@ fi
 
 echo "$SDK_Name version: $SDK_Version"
 Target_Branch=release/${SDK_Version}
+
 # current branch check
 Current_Branch=`git rev-parse --abbrev-ref HEAD`
 if [[ ${Current_Branch} != ${Target_Branch} ]]; then
